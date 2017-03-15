f7674b57276897b2e4b3e8a81f2ff3db65cdfbef
mithileshrane007 <mithilesh.r@infiny.in>
2017-03-15 16:31:27 +0530

    home changes

--- app/controllers/api/v1/targets_controller.rb
+++ app/controllers/api/v1/targets_controller.rb
@@ -248,52 +248,115 @@ class Api::V1::TargetsController < ApplicationController
 
  	    end
 
- 	    def log_hour
+ 	  #   def log_hour
  	    	
-	 			token = request.headers["token"]
-	 			target = Target.find_by_auth_token(token).id
-	 	    	date= params[:date]
-	 	    	time= params[:time]
-	 	    	target_id= params[:target_id]
-	 	    	is_start= params[:is_start]
-	 	    	is_stop= params[:is_stop]
+	 		# 	token = request.headers["token"]
+	 		# 	target = Target.find_by_auth_token(token).id
+	 	 #    	date= params[:date]
+	 	 #    	time= params[:time]
+	 	 #    	target_id= params[:target_id]
+	 	 #    	is_start= params[:is_start]
+	 	 #    	is_stop= params[:is_stop]
 
-	 	    if not is_start.blank? 
+	 	 #    if not is_start.blank? 
 
-	 	    	log = DayLog.find_by_target_id_and_date(target_id,date)
+	 	 #    	log = DayLog.find_by_target_id_and_date(target_id,date)
 	 	    	
-		 	    	puts log
-		 		if log.blank?
-		 			hour= DayLog.new(target_id: target,date: date,prev_time: time)
-		 			hour.save
-		 			data1 ={}
-					data1['error'] = 'false'
-					data1['msg'] = 'success'
-		 		else
-		 			puts "***********************"
-		 			log = DayLog.find_by_target_id_and_date(target_id,date)
+		 	#     	puts log
+		 	# 	if log.blank?
+		 	# 		hour= DayLog.new(target_id: target,date: date,prev_time: time)
+		 	# 		hour.save
+		 	# 		data1 ={}
+				# 	data1['error'] = 'false'
+				# 	data1['msg'] = 'success'
+		 	# 	else
+		 	# 		puts "***********************"
+		 	# 		log = DayLog.find_by_target_id_and_date(target_id,date)
 		 			
-		 	    	puts log
-		 			log.prev_time= time
-		 			puts "99999999999999999999999999999999999999"
-		 			puts log.prev_time
-		 			log.save
-		 			data1 ={}
-					data1['error'] = 'false'
-					data1['msg'] = 'success'
-		 		end
-		 	else
-		 			data1 ={}
-					data1['error'] = 'true'
-					data1['msg'] = 'unsuccessful'
-		 	end	
-		 			respond_to do |format|
-			    		format.json { render json: data1 }
-		        	end
+		 	#     	puts log
+		 	# 		log.prev_time= time
+		 	# 		puts "99999999999999999999999999999999999999"
+		 	# 		puts log.prev_time
+		 	# 		log.save
+		 	# 		data1 ={}
+				# 	data1['error'] = 'false'
+				# 	data1['msg'] = 'success'
+		 	# 	end
+		 	# else
+		 	# 		data1 ={}
+				# 	data1['error'] = 'true'
+				# 	data1['msg'] = 'unsuccessful'
+		 	# end	
+		 	# 		respond_to do |format|
+			 #    		format.json { render json: data1 }
+		  #       	end
 
 
 
 
- 	    end
+ 	  #   end
+ 	  def log_hour
+ 	  	def log_hour
+            
+                token = request.headers["token"]
+                target = Target.find_by_auth_token(token).id
+                date= params[:date]
+                time= params[:time]
+                target_id= params[:target_id]
+                is_start= params[:is_start]
+                is_stop= params[:is_stop]
+            begin
+                
+            
+                if not is_start.blank? 
+                    log = DayLog.find_by_target_id_and_date(target_id,date)
+                    
+                        puts log
+                    if log.blank?
+                        hour= DayLog.new(target_id: target,date: date,prev_time: time)
+                        hour.log_hour = 0
+                        hour.save
+                        data1 ={}
+                        data1['error'] = 'false'
+                        data1['msg'] = 'success'
+                    else
+                        puts "***********************"
+                        log = DayLog.find_by_target_id_and_date(target_id,date)
+                        
+                        puts log
+                        log.log_hour = log.log_hour + (Time.parse(time) - Time.parse(log.prev_time))/3600
+                        puts log.log_hour
+                        log.prev_time = time
+                        puts "99999999999999999999999999999999999999"
+                        puts log.prev_time
+                        log.save
+                        data1 ={}
+                        data1['error'] = 'false'
+                        data1['msg'] = 'success'
+                    end
+                else
+                        log = DayLog.find_by_target_id_and_date(target_id,date)
+                        puts log
+                        log.log_hour = log.log_hour + (Time.parse(time) - Time.parse(log.prev_time))/3600
+                        puts log.log_hour
+                        log.prev_time = time
+                        puts "99999999999999999999999999999999999999"
+                        puts log.prev_time
+                        log.save
+                        data1 ={}
+                        data1['error'] = 'false'
+                        data1['msg'] = 'success'
+                end 
+            rescue Exception => e
+            
+            data1 ={}
+            data1['error'] = 'true'
+            data1['msg'] = 'unsuccess'  
+            end
+            respond_to do |format|
+                format.json { render json: data1 }
+            end
+
+ 	  end
    
 end
